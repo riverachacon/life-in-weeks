d758c0423b18cb26ed11c8b498e6cba3edd42d19
Giovanni Rivera Chacon <princecito@Giovannis-Air.lan>
2024-07-31 18:34:15 -0400

    add onMouseEnter and onMouseLeave to calculate years and weeks

--- src/components/LifeInWeeks.jsx
+++ src/components/LifeInWeeks.jsx
@@ -3,8 +3,19 @@ import "./lifeInWeeks.css";
 function LifeInWeeks(props) {
   let weeksLived = props.onWeeksLived;
   let weeksLeft = props.onWeeksLeft;
+  let totalWeeks = weeksLived + weeksLeft;
   let birthday = props.onBirthday;
   const [showMessage, setShowMessage] = useState(true);
+  const [hoverInfo, setHoverInfo] = useState({ row: null, column: null });
+
+  function handleMouseEnter(index) {
+    let column = index % 52;
+    let row = Math.floor(index / 52);
+    setHoverInfo({ row, column });
+  }
+  function handleMouseLeave() {
+    setHoverInfo({ row: null, column: null });
+  }
 
   useEffect(() => {
     if (birthday) {
@@ -15,18 +26,40 @@ function LifeInWeeks(props) {
   return (
     <>
       {showMessage ? ( // if showMessage true, show h2
-        <h2>Please enter your birthdate</h2>
+        <h2>Please enter your birthday</h2>
       ) : (
         // else show weeks
         <>
-          <h2>hola</h2>
+          <div className="instructions">
+            <div className="lived">
+              <div className="lived-color"></div> <p>WEEKS LIVED</p>
+            </div>
+            <div className="left">
+              <div className="left-color"></div> <p>WEEKS REMAINING</p>
+            </div>
+          </div>
+          <div className="info">
+            <p>Week: {hoverInfo.column}</p>
+            <p>Year: {hoverInfo.row}</p>
+          </div>
+
           <div className="weeks-container">
             <div className="weeks-count">
-              {Array.from({ length: weeksLived }).map((_, index) => (
-                <div key={index} className="weeks-lived"></div>
+              {Array.from({ length: weeksLived }).map((i, index) => (
+                <div
+                  key={index}
+                  onMouseEnter={() => handleMouseEnter(index)}
+                  onMouseLeave={handleMouseLeave}
+                  className="weeks-lived"
+                ></div>
               ))}
-              {Array.from({ length: weeksLeft }).map((_, index) => (
-                <div key={index} className="weeks-left"></div>
+              {Array.from({ length: weeksLeft }).map((i, index) => (
+                <div
+                  key={index}
+                  onMouseEnter={() => handleMouseEnter(index)}
+                  onMouseLeave={handleMouseLeave}
+                  className="weeks-left"
+                ></div>
               ))}
             </div>
           </div>
